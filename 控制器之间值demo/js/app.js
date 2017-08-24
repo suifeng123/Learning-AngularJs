d7fe6a0d7e05f9e0d5c07b7a24d501e7f549be40
suifeng123 <wswen1220@163.com>
2017-08-24 17:07:59 +0800

    对angularjs中指令的研究

--- 控制器之间值demo/js/app.js
+++ 控制器之间值demo/js/app.js
@@ -2,32 +2,18 @@
  * Created by Administrator on 2017/2/14.
  */
 angular.module('demoForNotify')
-  .controller('parentCtrl',function($rootScope,$scope){
-     //实现相同控制器之间的值得传递
-     $scope.message = "dasfdsasd";
-    // $rootScope.message = "hello world";
-  //   $scope.message = "hello111";
-  $rootScope.click = function(){
-     $rootScope.$broadcast('childValue',$scope.message);
-  }
+.directive('unordered-list',function(scope,elment,attrs){
+  console.log(scope);
+  console.log(element);
+   console.log(attrs);
+})
+  .controller('defaultCtrl',function($rootScope,$scope){
+    $scope.products = [
+   {name:'Apples',category:'Fruit',price:1.20,expiry:10},
+   {name:'Banane',category:'Fruit',price:1.20,expiry:10},
+   {name:'Pears',category:'Fruit',price:1.20,expiry:10},
+    ];
 
 
   })
-  .controller('childCtrl',function($rootScope,$scope){
-     $scope.message = "";
-     //console.log($rootScope)
-     $scope.$on('childValue',function(d,data){
-     	console.log("我们现在取到了数据");
-     	console.log(data);
-     	console.log(d);
-     	console.log("999999");
-     	$scope.message = data;
-     })
-  })
-  .controller('otherCtrl',function($rootScope,$scope){
-
-      $rootScope.$on('childValue',function(d,data){
-         $scope.message = data;
-
-      })
-  })
+  
