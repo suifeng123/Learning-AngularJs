585ea3b65d5c3ae9df7899c727eee2065219dcbc
suifeng123 <wswen1220@163.com>
2017-08-25 17:35:43 +0800

    提交angularJS的源码的理解

--- forEach.js
+++ forEach.js
@@ -56,3 +56,121 @@ function isArray(obj){
 	//判断一个数组是否是数组对象
 	return Object.prototype.toString.apply(value) == '[object Array]';
 }
+
+//修复IE中没有trim的缺点
+var trim = (function(){
+	if(!String.prototype.trim){
+        return funtion(value){
+        	return isString(value) ? value.replace(/^\s*/,"").replace(/\s*$/,'')
+        };
+	}
+
+	return function(value){
+         return isString(value)?Object.prototype.trim.call(value):value;
+	}
+})();  //立即执行
+
+function isString(obj){
+	return typeof obj === 'string';
+}
+
+function isElement(node){
+	return node && node.find && (node.name || (node.find && node.on));
+}
+
+//写一个函数，将一个以逗号隔开的字符串弄成一个map
+function makeMap(str){
+	var obj = {};//定义一个对象
+	var items = str.split(',');
+	var i;
+
+	for(i = 0 ; i < items.length ; i++){
+		obj[items[i]] = items[i];
+	}
+	return obj;
+}
+
+
+function isObject(obj){
+	return typeof obj === 'object';
+}
+function size(obj,ownPropsOnly){
+	var size = 0,key;
+
+	if(isArray(obj) || isString(obj)){
+		return obj.length;
+	}else if(isObject(obj)){
+		for(key in obj){
+			if(!ownPropsOnly || obj.hasOwnProperty(key)) size++;
+		}
+	}
+
+   return size;
+}
+
+function include(array,obj){
+	return indexOf(array,obj);
+}
+
+function indexOf(array,obj){
+	if(array.indexOf) return array.indexOf(obj);
+
+	for(var i = 0 ; i < array.length ; i++){
+		if(obj === array[i]) return  i;
+	}
+	return -1;
+}
+
+function removeArray(array,obj){
+	var index = indexOf(array,obj);  //获取obj在对象中的位置
+	if(index > 0)
+		array.splice(index,1);
+	return obj;
+}
+
+function isLeafNode(node){
+	if(node){
+		switch(node.nodeName){
+			case 'OPTION':
+			case "PRE":
+			case 'TITLE':
+			return true;
+		}
+	}
+	return false;
+}
+
+function copy(source,desitination){
+	if(isWindow(source) || isScope(source)){
+		throw Error("source is can not copy")
+	}
+
+	if(!desitination){
+		source = desitination;
+		if(source){
+          if(isArray(source)){
+          	desitination = copy(source,[]);
+          }else if(isDate(source)){
+          	desitination = new Date(source.getTime());
+          }else if(isRegExp(source)){
+          	desitination = new RegExp(source.source);
+          }else if(isObject(source)){
+          	desitination = copy(source,{});
+          }
+
+		}
+	}else{
+		if(source === desitination){
+			throw Error("source is the same with the desitination");
+		}
+
+		if(isArray(source)){
+			desitination.length = 0;
+			//下面开始遍历,如果是单个没有特殊的个体，只需直接复制
+			for(var i = 0 ; i < source.length ; i++){
+				desitination.push(copy(source[i]));
+			}
+		}
+	}
+}
+
