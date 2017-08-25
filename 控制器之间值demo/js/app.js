f28578b2f25e62c7955025fa31d4a606c45e0b35
suifeng123 <wswen1220@163.com>
2017-08-25 08:45:43 +0800

    提交关于

--- 控制器之间值demo/js/app.js
+++ 控制器之间值demo/js/app.js
@@ -2,12 +2,29 @@
  * Created by Administrator on 2017/2/14.
  */
 angular.module('demoForNotify')
-.directive('unordered-list',function(scope,elment,attrs){
+.directive('unorderedList',function(){
+ 
+ return function(scope,element,attrs){
   console.log(scope);
   console.log(element);
-   console.log(attrs);
+  console.log(attrs);
+   console.log(angular);
+   var data = scope[attrs['unorderedList']];
+   var propertyExpression = attrs['listProperty'];
+  if(angular.isArray(data)){
+    var listElem = angular.element('<ul>');
+    element.append(listElem);
+    for(var i = 0 ; i < data.length ; i++){
+      var itemElement = angular.element("<li>")
+         .text(scope.$eval(propertyExpression,data[i]));
+         listElem.append(itemElement);
+    }
+  }
+}
 })
   .controller('defaultCtrl',function($rootScope,$scope){
+    console.log("展示");
+    console.log($scope);
     $scope.products = [
    {name:'Apples',category:'Fruit',price:1.20,expiry:10},
    {name:'Banane',category:'Fruit',price:1.20,expiry:10},
